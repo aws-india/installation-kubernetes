eb6309236cd933d1c5a67d9e8afd94f92d523dc8
Developer Team <bhimprakashsingh@gmail.com>
2023-07-24 07:37:12 +0530

    Create kubernetes.sh

---
+++ kubernetes.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+
+sleep 5s
+
+sudo apt-get install -y ca-certificates curl
+sleep 5s
+sudo apt-get install -y apt-transport-https
+sleep 5s
+
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+sleep 30s
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sleep 5s
+sudo apt-get install -y kubectl
+
+
