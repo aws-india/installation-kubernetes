d1a0ef803451d69fd4e265f90bb7a8e14dd5e43f
Developer Team <bhimprakashsingh@gmail.com>
2023-07-24 08:43:49 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -32,24 +32,24 @@ sudo usermod -aG docker ${USER}
 
 
 
-sudo apt-get update && apt-get install -y apt-transport-https curl
-
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-
-sleep 30s
-
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-
-sudo apt-get update
+sudo apt-get update 
 
 echo ".....................Installing kubeadm kubelet kubectl"
 
-sudo apt-get install -y kubeadm=1.10.5-00 kubectl=1.10.5-00 kubelet=1.10.5-00
+sudo apt-get install -y ca-certificates curl
+sleep 5s
+sudo apt-get install -y apt-transport-https
 
-apt-mark hold kubelet kubeadm kubectl
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+sleep 30s
 
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+
+sudo apt-get install -y kubeadm
+sudo apt-get install -y kubectl
+sudo apt-get install -y kubelet
 
 echo ".......................Restart kubelet"
 
@@ -59,7 +59,3 @@ systemctl restart kubelet
 sudo apt-get update
 
 echo ".......................Execute sucessfully"
-
-
-
-
